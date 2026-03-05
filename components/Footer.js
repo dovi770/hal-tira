41a29bbf6a07a693520cefd1ba56d114e1a87c9b
dovi770 <770dovi770@gmail.com>
2026-03-05 22:37:22 +0200

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>© כל הזכויות שמורות</p>
+
+      <style jsx>{`
+        .footer {
+          padding: 40px;
+          text-align: center;
+          background: #020617;
+          color: white;
+        }
+      `}</style>
+    </footer>
+  );
+}
